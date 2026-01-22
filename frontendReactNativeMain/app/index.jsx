ad4f0acf3f7176f2bcfb4211051be8f21afc5ff4
Lintes Claudiu <claudiu.lintes@student.upt.ro>
2026-01-22 03:41:11 +0200

    Updated frontend to use router

---
+++ frontendReactNativeMain/app/index.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+const Home = () => {
+    return (
+        <View>
+            <Text>HomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHome</Text>
+        </View>
+    )
+}
+
+export default Home
+
+const styles = StyleSheet.create({})
